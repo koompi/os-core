d3025ee04cff41f654150f6ba889c392d198eda3
honghangsia <honghangsia@gmail.com>
2020-09-27 13:05:42 +0700

    give credit

--- res/version-check.sh
+++ res/version-check.sh
@@ -1,4 +1,6 @@
 #!/bin/bash
+# Author HangsiaHONG <hangsia@koompi.org>
+
 # Simple script to list version numbers of critical development tools
 export LC_ALL=C
 bash --version | head -n1 | cut -d" " -f2-4
