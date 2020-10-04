6996e191bc411a490d60bfa91741e57574c24637
hangsiahong <honghangsia@gmail.com>
2020-10-04 14:33:24 +0700

    fixed alot of things

--- 3_runthis.sh
+++ 3_runthis.sh
@@ -1,10 +1,9 @@
 #!/bin/bash
 # Author < HangsiaHONG hangsia@koompi.org >
 
-## Run the script inside Structure folder from 05 - _
 
 CWD=$PWD
-#source $CWD/structure/config
+source $CWD/structure/config
 
 # function setup_env() {
 #     bash $CWD/structure/05_setup_env.sh
--- structure/01_create_partition.sh
+++ structure/01_create_partition.sh
@@ -1,46 +1,27 @@
 #!/bin/bash
 # Author < HangsiaHONG hangsia@koompi.org >
 
-
 CWD=$PWD
 source $CWD/structure/config
 
-Green='\033[0;32m'  # Green
-
-echo -e "${Green}Enter your disk name, Ex: sda, nvme0n1 by type cmd lsblk"
-read disk
-
-
-Disk=/dev/$disk
-
 
 function create_part() {
-sudo parted --script $Disk \
-mklabel gpt \
-mkpart primary ext2 1Mib 101Mib \
-mkpart primary ext4 101Mib 5G \
-mkpart prmary ext4 5G 20G
-
+    sudo parted --script /dev/sdb \
+    mklabel gpt \
+    mkpart primary ext2 1Mib 101Mib \
+    mkpart primary ext4 101Mib 5G \
+    mkpart prmary ext4 5G 20G
 }
 
 function format_part() {
-mkfs -v -t ext2 $Disk\1
-mkswap $Disk\2
-mkfs -v -t ext4 $Disk\3
+    mkfs -v -t ext2 /dev/sdb1
+    mkswap /dev/sdb2
+    mkfs -v -t ext4 /dev/sdb3
 }
 
 function mount_part() {
-mkdir -pv $KFS
-mount /dev/$Disk\3 $KFS
-/sbin/swapon -v $Disk\2 
+    mkdir -pv $KFS
+    mount /dev/sdb3 $KFS
+    /sbin/swapon -v /dev/sdb2
 }
 
-
-
-function main() {
-	create_part &&
-	format_part &&
-	mount_part
-}
-
-main
