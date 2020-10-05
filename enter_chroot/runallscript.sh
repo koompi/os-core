55014d38b51b0f65a639c1188139a7ea3a09f18c
hangsiahong <honghangsia@gmail.com>
2020-10-05 12:39:13 +0700

    make runallscript inside enter_chroot folder

---
+++ enter_chroot/runallscript.sh
@@ -0,0 +1,16 @@
+#!/bin/bash
+# Author < HangsiaHONG hangsia@koompi.org >
+
+CWD=$PWD
+source $CWD/cross_toolchain/config
+
+function enter_chroot() {
+    bash $CWD/enter_chroot/1_change_ownership.sh &&
+    bash $CWD/enter_chroot/2_create_virtual_kernel_filesystem.sh &&
+    bash $CWD/enter_chroot/3_enter_chroot_env.sh &&
+    bash $CWD/enter_chroot/4_create_directories.sh &&
+    bash $CWD/enter_chroot/5_create_file_and_symlinks.sh
+}
+
+
+enter_chroot
