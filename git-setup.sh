4a3485dfe42c65f6266e75ad3f431600d67dbbd8
hangsiahong <honghangsia@gmail.com>
2021-01-26 13:23:22 +0700

    reset

--- git-setup.sh
+++
@@ -1,5 +0,0 @@
-#!/bin/bash
-
-
-git config --global user.email "honghangsia@gmail.com"
-git config --global user.name "hangsiahong"
--- scripts/update-kernel.sh
+++
@@ -1,5 +0,0 @@
-cp /mnt/kfs/boot/vmlinuz-kfs /mnt/kfs/boot/vmlinuz  && \
-cp kmp-files/livecd.hook /mnt/kfs/etc/mkinitramfs.d && \
-kernver=$(file /mnt/kfs/boot/vmlinuz-kfs | cut -d ' ' -f9) && \
-chroot_run mkinitramfs -k $kernver -a livecd -o /boot/initrd-live.img && \
-mv /mnt/kfs/boot/initrd-live.img /mnt/kfs/tmp/boot/initrd 
